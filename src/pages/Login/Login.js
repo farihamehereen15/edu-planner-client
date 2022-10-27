a13d8b9b7902cbde06bc5d152347c2c5558db7d6
farihamehereen15 <fariha15-12875@diu.edu.bd>
2022-10-27 16:47:57 +0600

    register done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -38,34 +38,7 @@ const Login = () => {
     }
     return (
 
-        <div className='container w-50 my-5'>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control name="email" type="email" placeholder="Enter email" required />
-
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control name="password" type="password" placeholder="Password" required />
-                </Form.Group>
-
-                <Button variant="primary" type="submit" >
-                    Submit
-                </Button> <br />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text> <br />
-                <Button onClick={handleGoogleSignIn} variant="outline-primary" type="submit" className='my-1' >
-                    Log In with Google
-                </Button> <br />
-                <Button variant="outline-primary" type="submit" >
-                    Log In with Github
-                </Button>
-            </Form>
-
-        </div>
+       
 
     );
 }
